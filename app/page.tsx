87aa7855d6326f55e1a7832543def2e481b33ce8
susanbeardsley <susan@stylebundle.com.au>
2026-05-02 23:14:47 +1000

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '40px', fontFamily: 'system-ui' }}>
+      <h1>🎁 Style Bundle - Gifting Insights Engine</h1>
+      <p>If you see this, deployment works!</p>
+    </main>
+  )
+}
